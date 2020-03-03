7f078515e10389ed911f570ff8b9b27774f28774
zbettmen <rhamula@hotmail.com>
2020-03-03 18:39:24 +0100

    first commit on exer 3

--- src/main/java/com/example/Recipe_db/ComandLine.java
+++ src/main/java/com/example/Recipe_db/ComandLine.java
@@ -21,6 +21,9 @@ public class ComandLine implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         ingridentRepository.save(new Ingredient("Bruce Wayne"));
-
+        System.out.println(ingridentRepository.findByIngredientName("Bruce Wayne"));
     }
+
+
+
 }
--- src/main/java/com/example/Recipe_db/Data/IngridentRepository.java
+++ src/main/java/com/example/Recipe_db/Data/IngridentRepository.java
@@ -1,12 +1,20 @@
 package com.example.Recipe_db.Data;
 
 import com.example.Recipe_db.entity.Ingredient;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IngridentRepository  extends CrudRepository<Ingredient,Integer> {
 
     Optional<Ingredient> findByIngredientName(String ingredientName);
+
+    @Query("SELECT i FROM Ingredient i WHERE i.ingredientName LIKE %:ingredientName%")
+    List<Ingredient> findByIngridentByNameContains(@Param("ingredientName") String ingredientName);
+
+
 
 }
